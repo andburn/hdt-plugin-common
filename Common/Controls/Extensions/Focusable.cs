807619e3c404179347ae86876f8d0a09edf3a602
Andrew Burnett <andrew@andburn.info>
2017-02-24 18:36:37 +0000

    Add watermark adorner

---
+++ Common/Controls/Extensions/Focusable.cs
@@ -0,0 +1,38 @@
+﻿/*
+ *  Focusing a UIElement from a view model
+ *  Source: http://stackoverflow.com/a/1356781
+ */
+
+namespace HDT.Plugins.Common.Controls.Extensions
+{
+	using System.Windows;
+
+	public static class Focusable
+	{
+		public static bool GetIsFocused(DependencyObject obj)
+		{
+			return (bool)obj.GetValue(IsFocusedProperty);
+		}
+
+		public static void SetIsFocused(DependencyObject obj, bool value)
+		{
+			obj.SetValue(IsFocusedProperty, value);
+		}
+
+		public static readonly DependencyProperty IsFocusedProperty =
+			DependencyProperty.RegisterAttached(
+				"IsFocused", typeof(bool), typeof(Focusable),
+				new UIPropertyMetadata(false, OnIsFocusedPropertyChanged));
+
+		private static void OnIsFocusedPropertyChanged(
+			DependencyObject d,
+			DependencyPropertyChangedEventArgs e)
+		{
+			var uie = (UIElement)d;
+			if ((bool)e.NewValue)
+			{
+				uie.Focus(); // false values don't matter in this case
+			}
+		}
+	}
+}
