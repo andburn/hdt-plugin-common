beeadc7946ddec4a6bd8a76e489e30decf0dc2d9
Andrew Burnett <andrew@andburn.info>
2017-04-19 18:03:47 +0100

    Move git versioning here from plugins

---
+++ Common/Utils/GitVersion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace HDT.Plugins.Common.Utils
+{
+	public class GitVersion
+	{
+		public static Version Get(Assembly assembly, object obj)
+		{
+			if (assembly == null)
+				return null;
+
+			var nspace = obj.GetType().Namespace;
+			var gitInfo = assembly.GetType(nspace + ".GitVersionInformation");
+			var semVer = gitInfo?.GetField("AssemblySemVer").GetValue(null).ToString();
+			Version result = null;
+			Version.TryParse(semVer, out result);
+
+			return result;
+		}
+	}
+}
