2b185fcf9e558f1708f255cb4c26b269310b27f7
Andrew Burnett <andrew@andburn.info>
2017-06-24 11:14:48 +0100

    Add string case converter

---
+++ Common/Utils/CaseConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace HDT.Plugins.Common.Utils
+{
+	/// <summary>
+	/// Convert a string to all upper or lower case.
+	/// </summary>
+	/// <remarks>based on https://stackoverflow.com/a/29841405</remarks>
+	public class CaseConverter : IValueConverter
+	{
+		// change in xaml with:
+		// <local:CaseConverter Case="Lower" x:Key="CaseConverter"/>
+		public CharacterCasing Case { get; set; }
+
+		public CaseConverter()
+		{
+			Case = CharacterCasing.Upper;
+		}
+
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			var str = value as string;
+			if (str != null)
+			{
+				switch (Case)
+				{
+					case CharacterCasing.Lower:
+						return str.ToLower();
+
+					case CharacterCasing.Upper:
+						return str.ToUpper();
+
+					default:
+						return str;
+				}
+			}
+			return string.Empty;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
